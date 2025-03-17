e98c445a9406c88b27dca200affee76eaeda5b38
MaxSlipchuk <nice.slipchukmax@gmail.com>
2025-03-17 20:33:29 +0200

    create register form

--- users/forms.py
+++ users/forms.py
@@ -20,3 +20,23 @@ class LoginUserForm(forms.Form):
     #     # отримати поточну модель користувача
     #     user = get_user_model()
     #     fields = ['username', 'password']
+
+
+
+class RegisterUserForm(forms.ModelForm):
+    username = forms.CharField(label='Логін', 
+                               widget = forms.TextInput(attrs={'class': 'form-input'}))
+    email = forms.EmailField(label='E-mail',
+                             widget = forms.EmailInput(attrs={'class': 'form-input'}))
+
+    password = forms.CharField(label='Пароль', 
+                               widget = forms.PasswordInput(attrs={'class': 'form-input'}))
+    password2 = forms.CharField(label='Підтвердження пароля', 
+                               widget = forms.PasswordInput(attrs={'class': 'form-input'}))
+
+    class Meta:
+        user = get_user_model()
+        fields = ['username', 'email', 'password', 'password2']
+        labels = {
+            'email': 'E-mail'
+        }
